99dc44d3c11da783d2737adb2745bd59ec4f1683
AndersMarkoff <105625756+AndersMarkoff@users.noreply.github.com>
2025-08-14 10:31:21 +0200

    License info in source files

--- mcParkingStockholm.py
+++ mcParkingStockholm.py
@@ -1,3 +1,22 @@
+'''
+MC Parking Stockholm - A script that generate a GPX file with all
+motorcycle parking spots in Stockholm.
+Copyright (C) 2025 Anders Markoff
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 import os
 from dotenv import load_dotenv
 import mcParking
